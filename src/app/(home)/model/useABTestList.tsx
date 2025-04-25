ffbe4ae08d93db93ade0c82cd4a2fa9b96dbd0a9
jch1223 <ico1828@gmail.com>
2025-04-25 17:13:39 +0900

    list type에 대한  ab test 로직 구현

---
+++ src/app/(home)/model/useABTestList.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import { useLayoutEffect, useState } from 'react';
+
+const getRandomListType = () => {
+  const random = Math.random();
+  let listType: 'list' | 'grid' = 'list';
+
+  if (random < 0.5) {
+    listType = 'grid';
+  }
+
+  return listType;
+};
+
+export const useABTestList = () => {
+  const [listType, setListType] = useState<'list' | 'grid'>('list');
+
+  useLayoutEffect(() => {
+    if (!localStorage.getItem('listType')) {
+      const newType = getRandomListType();
+      localStorage.setItem(
+        'listType',
+        JSON.stringify({
+          listType: newType,
+          createdAt: new Date().getTime(),
+        })
+      );
+      setListType(newType);
+      return;
+    }
+
+    const stored = JSON.parse(localStorage.getItem('listType') || '{}');
+
+    // 24시간이 지나지 않은 경우 - 저장된 값 사용
+    if (new Date().getTime() - stored.createdAt < 24 * 60 * 60 * 1000) {
+      setListType(stored.listType);
+      return;
+    }
+
+    // 24시간이 지난 경우 - 새 값 설정
+    const newType = getRandomListType();
+    localStorage.setItem(
+      'listType',
+      JSON.stringify({
+        listType: newType,
+        createdAt: new Date().getTime(),
+      })
+    );
+
+    setListType(newType);
+  }, []);
+
+  return { listType };
+};
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,14 @@
+'use client';
+
 import { SearchInput } from '@/features/productsFilter/ui/searchInput';
 import { OrderByRatingSelect } from '@/features/productsFilter/ui/orderByRatingSelect';
 import { Catalog } from '@/widgets/catalog/ui/catalog';
 import { Suspense } from 'react';
+import { useABTestList } from './model/useABTestList';
 
 export default function Home() {
+  const { listType } = useABTestList();
+
   return (
     <div>
       <div className="mb-10">
@@ -15,7 +20,7 @@ export default function Home() {
       </div>
 
       <Suspense fallback={<div>Loading...</div>}>
-        <Catalog type="grid" />
+        <Catalog type={listType} />
       </Suspense>
     </div>
   );
